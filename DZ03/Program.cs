ac362a5e175dae977d91445f6fe9f0cc1281b4cf
Vadim <sg_vadim@mail.ru>
2022-11-29 12:40:49 +0300

    Сделал DZ03

---
+++ DZ03/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int cub = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cub){
+  int coun = 0;
+  int length = cub.Length;
+  while (coun <  length){
+    cub[coun] = Convert.ToInt32(Math.Pow(coun, 3));
+    coun++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int coun = coll.Length;
+  int index = 0;
+  while(index < coun){
+    Console.WriteLine(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cub+1];
+Cube(arry);
+PrintArry(arry);
+
+
+
+
